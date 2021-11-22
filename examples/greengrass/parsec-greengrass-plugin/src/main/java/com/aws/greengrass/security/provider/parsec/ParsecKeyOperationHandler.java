4bcce83276b9f5e1c6248eb3c6d1a0437dda1af0
Sam Spycher <samspycher@users.noreply.github.com>
2021-11-22 21:13:12 +0100

    Add NONEwithRSA MessageDigest and incorporate typecast for Buffer methods

--- examples/greengrass/parsec-greengrass-plugin/src/main/java/com/aws/greengrass/security/provider/parsec/ParsecKeyOperationHandler.java
+++ examples/greengrass/parsec-greengrass-plugin/src/main/java/com/aws/greengrass/security/provider/parsec/ParsecKeyOperationHandler.java
@@ -1,5 +1,7 @@
 package com.aws.greengrass.security.provider.parsec;
 
+import com.aws.greengrass.logging.api.Logger;
+import com.aws.greengrass.logging.impl.LogManager;
 import lombok.RequiredArgsConstructor;
 import software.amazon.awssdk.crt.io.TlsHashAlgorithm;
 import software.amazon.awssdk.crt.io.TlsKeyOperation;
@@ -9,6 +11,7 @@ import software.amazon.awssdk.crt.io.TlsSignatureAlgorithm;
 import java.security.PrivateKey;
 import java.security.Provider;
 import java.security.Signature;
+import java.util.Arrays;
 
 /**
  *
@@ -17,11 +20,13 @@ import java.security.Signature;
 public class ParsecKeyOperationHandler implements TlsKeyOperationHandler {
     final PrivateKey key;
     final Provider provider;
+    final Logger logger = LogManager.getLogger(ParsecKeyOperationHandler.class);
 
     public void performOperation(TlsKeyOperation operation) {
         System.out.println("perform operation " + operation);
         try {
             if (operation.getType() != TlsKeyOperation.Type.SIGN) {
+                logger.error("Simple sample only handles SIGN operations");
                 throw new RuntimeException("Simple sample only handles SIGN operations");
             }
 
@@ -34,10 +39,11 @@ public class ParsecKeyOperationHandler implements TlsKeyOperationHandler {
             }
 
             byte[] digest = operation.getInput();
-            Signature rsaSign = Signature.getInstance("SHA256withRSA", provider);
+            Signature rsaSign = Signature.getInstance("NONEwithRSA", provider);
             rsaSign.initSign(key);
             rsaSign.update(digest);
             byte[] signatureBytes = rsaSign.sign();
+            logger.info("signed value: {} {}", Arrays.toString(signatureBytes), signatureBytes.length);
             operation.complete(signatureBytes);
 
         } catch (Exception ex) {
