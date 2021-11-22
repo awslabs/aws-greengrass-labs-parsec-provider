f019ff5fac8be6a5f67b96b943f61386641a56dc
Sam Spycher <samspycher@users.noreply.github.com>
2021-11-22 19:30:47 +0100

    Add NONEWithRsa and typecast buffer in WireHeader

--- examples/greengrass/parsec-greengrass-plugin/src/main/java/com/aws/greengrass/security/provider/parsec/ParsecCryptoKeyService.java
+++ examples/greengrass/parsec-greengrass-plugin/src/main/java/com/aws/greengrass/security/provider/parsec/ParsecCryptoKeyService.java
@@ -21,57 +21,65 @@ import software.amazon.awssdk.iot.AwsIotMqttConnectionBuilder;
 
 import javax.inject.Inject;
 import java.net.URI;
+import java.net.URISyntaxException;
 
 import static com.aws.greengrass.componentmanager.KernelConfigResolver.CONFIGURATION_CONFIG_KEY;
 
 @ImplementsService(name = ParsecCryptoKeyService.PARSEC_SERVICE_NAME, priority = 0, autostart = true)
 public class ParsecCryptoKeyService extends PluginService implements CryptoKeySpi, MqttConnectionSpi {
 
-    public static final String PARSEC_SERVICE_NAME = "aws.greengrass.crypto.ParsecProvider";
-    public static final String PARSEC_SOCKET_TOPIC = "parsecSocket";
+  public static final String PARSEC_SERVICE_NAME = "aws.greengrass.crypto.ParsecProvider";
+  public static final String PARSEC_SOCKET_TOPIC = "parsecSocket";
 
-    @Delegate
-    private final ParsecCryptoKeysSpi parsecCryptoKeysSpi;
+  @Delegate
+  private final ParsecCryptoKeysSpi parsecCryptoKeysSpi;
+  private final SecurityService securityService;
 
-    @Inject
-    public ParsecCryptoKeyService(Topics topics,
-                                  SecurityService securityService,
-                                  DeviceConfiguration deviceConfiguration) {
-        super(topics);
-        this.parsecCryptoKeysSpi = new ParsecCryptoKeysSpi();
-        this.config.lookup(CONFIGURATION_CONFIG_KEY, PARSEC_SOCKET_TOPIC).subscribe(this::updateSocket);
+  @Inject
+  public ParsecCryptoKeyService(Topics topics,
+                                SecurityService securityService,
+                                DeviceConfiguration deviceConfiguration) {
+    super(topics);
+    this.securityService = securityService;
+    this.parsecCryptoKeysSpi = new ParsecCryptoKeysSpi();
+    this.config.lookup(CONFIGURATION_CONFIG_KEY, PARSEC_SOCKET_TOPIC).subscribe(this::updateSocket);
 
-        try {
-            securityService.registerCryptoKeyProvider(this);
-            securityService.registerMqttConnectionProvider(this);
-            this.parsecCryptoKeysSpi.afterRegistration(deviceConfiguration);
-        } catch (ServiceProviderConflictException e) {
-            throw new RuntimeException("Provider parsec already registered");
-        }
+    try {
+      securityService.registerCryptoKeyProvider(this);
+      securityService.registerMqttConnectionProvider(this);
+      this.parsecCryptoKeysSpi.afterRegistration(deviceConfiguration);
+    } catch (ServiceProviderConflictException e) {
+      throw new RuntimeException("Provider parsec already registered");
     }
+  }
 
 
-    private void updateSocket(WhatHappened what, Topic topic) {
-        if (topic != null && what != WhatHappened.timestampUpdated) {
-            this.parsecCryptoKeysSpi.setParsecSocketPath(Coerce.toString(topic));
-            if (!parsecCryptoKeysSpi.initializeParsecProvider()) {
-                serviceErrored("Can't initialize Parsec JCA provider when socket update");
-            }
-        }
+  private void updateSocket(WhatHappened what, Topic topic) {
+    if (topic != null && what != WhatHappened.timestampUpdated) {
+      this.parsecCryptoKeysSpi.setParsecSocketPath(Coerce.toString(topic));
+      if (!parsecCryptoKeysSpi.initializeParsecProvider()) {
+        serviceErrored("Can't initialize Parsec JCA provider when socket update");
+      }
     }
+  }
 
-    @Override
-    public AwsIotMqttConnectionBuilder getMqttConnectionBuilder(URI privateKeyUri, URI certificateUri) throws ServiceUnavailableException, MqttConnectionProviderException {
-                try {
-            ParsecKeyOperationHandler myKeyOperationHandler = new ParsecKeyOperationHandler(
-                    parsecCryptoKeysSpi.getKeyPair(privateKeyUri, certificateUri).getPrivate(),
-                    parsecCryptoKeysSpi.getParsecProvider()
-            );
-            TlsContextCustomKeyOperationOptions keyOperationOptions = new TlsContextCustomKeyOperationOptions(myKeyOperationHandler)
+  @Override
+  public AwsIotMqttConnectionBuilder getMqttConnectionBuilder(URI privateKeyUri, URI certificateUri) throws ServiceUnavailableException, MqttConnectionProviderException {
+    try {
+      /*ParsecURI key = new ParsecURI(privateKeyUri);
+      URI standardKey = new URI(key.getImport());
+      ParsecURI cert = new ParsecURI(certificateUri);
+      URI standardCert = new URI(cert.getImport());
+      return securityService.getMqttConnectionBuilder(standardKey, standardCert);*/
+      /* FIXME: native code throws segfault
+      */
+      ParsecKeyOperationHandler myKeyOperationHandler = new ParsecKeyOperationHandler(parsecCryptoKeysSpi.getKeyPair(privateKeyUri, certificateUri).getPrivate(), parsecCryptoKeysSpi.getParsecProvider());
+      TlsContextCustomKeyOperationOptions keyOperationOptions = new TlsContextCustomKeyOperationOptions(myKeyOperationHandler)
                 .withCertificateFilePath(ParsecURI.validateParsecURI(certificateUri).getImport());
-            return AwsIotMqttConnectionBuilder.newMtlsCustomKeyOperationsBuilder(keyOperationOptions);
-        } catch (KeyLoadingException e) {
-            throw new MqttConnectionProviderException("Failed to load Parsec key.", e);
-        }
+      return AwsIotMqttConnectionBuilder.newMtlsCustomKeyOperationsBuilder(keyOperationOptions);
+
+    } catch (KeyLoadingException e) {
+      throw new MqttConnectionProviderException("Failed to load Parsec key.", e);
     }
+  }
 }
